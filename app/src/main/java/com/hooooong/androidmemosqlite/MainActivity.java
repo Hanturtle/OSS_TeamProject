9a3405533eece6613c70d5c9124a5952d78838ab
Hanturtle <tjfdnjs0829@naver.com>
2021-01-20 14:13:19 +0900

    mainActivity 수정

--- app/src/main/java/com/hooooong/androidmemosqlite/MainActivity.java
+++ app/src/main/java/com/hooooong/androidmemosqlite/MainActivity.java
@@ -1,229 +1,95 @@
 package com.hooooong.androidmemosqlite;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import com.hooooong.androidmemosqlite.domain.Memo;
-import com.hooooong.androidmemosqlite.domain.MemoDAO;
+public class MainActivity extends AppCompatActivity {
 
-import java.util.ArrayList;
-
-/**
- * 안드로이드 SQLite 사용하기
- * <p>
- * 1. db 파일을 직접 코드로 생성
- * 2. 로컬에서 만든 파일을 assets 에 담은 후 복사/붙여넣기를 할 수 있다.
- * > 우편번호처럼 기반 데이터가 필요한 DB일 경우
- */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    MemoDAO memoDAO;
-
-    EditText editTitle, editContent;
-    Button btnCreate, btnRead, btnUpdate, btnDelete;
-    TextView textResult;
+    public static final int REQUEST_CODE_INSERT = 1000;
+    private MemoAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-        initListener();
-        init();
-    }
 
-    /**
-     * memoDAO 를 닫아준다.
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (memoDAO != null) {
-            memoDAO.close();
-        }
-    }
+        ListView listView = findViewById(R.id.memo_list);
 
-    /**
-     * onClick 정의
-     * @param v
-     */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnCreate:
-                createAfterRead();
-                break;
-            case R.id.btnRead:
-                read();
-                break;
-            case R.id.btnUpdate:
-                updateAfterRead();
-                break;
-            case R.id.btnDelete:
-                deleteAfterRead();
-                break;
-        }
-    }
+        //리싸이클러뷰로 보여지지않고 리스트로 보여지므로 커서를 사용하여 선택한 id에 해당하는 데이터들의 작업이 이루어지게함
+        Cursor cursor = getMemoCursor();
 
-    /**
-     * 화면에 작성한 값들을 Memo 객체로 생성
-     *
-     * @return
-     */
-    private Memo getMemoFormScreen() {
-        // 1. 화면에 입력된 값을 가져온다.
-        String title = editTitle.getText().toString();
-        String content = editContent.getText().toString();
-        // 2. Memo 객체를 하나 생성하여 값을 담는다.
-        Memo memo = new Memo(title, content);
-        return memo;
-    }
+        mAdapter = new MemoAdapter(this, cursor);
+        listView.setAdapter(mAdapter);
 
-    /**
-     * EditText 초기화
-     */
-    private void resetScreen() {
-        editTitle.setText("");
-        editContent.setText("");
-        // 초기화 한 후 focus 준다.
-        editTitle.requestFocus();
-    }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, MemoActivity.class);
 
-    /**
-     * Toast 출력
-     *
-     * @param message
-     */
-    private void showInfo(String message) {
-        // Toast Message 출력
-        Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
-    }
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(i);
+                String title = cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_TITLE));
+                String contents = cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COMLUMN_NAME_CONTENTS));
 
-    /**
-     * CREATE 실행
-     */
-    public void create(Memo memo) {
-        // DB 실행
-        memoDAO.create(memo);
-    }
+                intent.putExtra("id", l);
+                intent.putExtra("title", title);
+                intent.putExtra("contents", contents);
 
-    /**
-     * READ 실행
-     */
-    public void read() {
-        // 0. 쿼리 생성
-        // 1. DB 실행한 후 결과값을 받아온다.
-        ArrayList<Memo> memoList = memoDAO.read(new String[]{"id", "title", "content", "nDate"}, null);
-        // 2. 결과값 출력
-        textResult.setText("");
-        if (memoList.size() != 0) {
-            for (Memo memo : memoList) {
-                textResult.append(memo.toString()+"\n");
+                startActivityForResult(intent, MainActivity.REQUEST_CODE_INSERT);
             }
-        }
+        });
 
     }
 
-    /**
-     * UPDATE 실행
-     */
-    public void update(Memo memo) {
-        // DB 실행
-        memoDAO.update(memo);
+    private Cursor getMemoCursor() {
+        MemoDbHelper helper = MemoDbHelper.getInstance(this);
+        return helper.getReadableDatabase()
+                .query(MemoContract.MemoEntry.TABLE_NAME
+                        ,null, null, null, null, null, MemoContract.MemoEntry._ID + " DESC");
     }
 
-    /**
-     * DELETE 실행
-     */
-    public void delete() {
-        // DB 실행
-        memoDAO.delete();
-    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    /**
-     * CREATE 후 처리
-     */
-    private void createAfterRead() {
-        Memo memo = getMemoFormScreen();
-        // 1. 생성
-        create(memo);
-        // 2. 결과 완료
-        showInfo("등록 완료!");
-        // 3. 목록 갱신
-        resetScreen();
-        read();
-    }
-
-    /**
-     * UPDATE 후 처리
-     */
-    private void updateAfterRead() {
-        Memo memo = getMemoFormScreen();
-        // 1. 생성
-        update(memo);
-        // 2. 결과 완료
-        showInfo("수정 완료!");
-        // 3. 목록 갱신
-        resetScreen();
-        read();
-    }
-
-    /**
-     * DELETE 후 처리
-     */
-    private void deleteAfterRead() {
-        ArrayList<Memo> memoList = memoDAO.read(new String[]{"id", "title", "content", "nDate"}, null);
-
-        if (memoList.size() != 0) {
-            delete();
-            // 2. 결과 완료
-            showInfo("삭제 완료!");
-            // 3. 목록 갱신
-            resetScreen();
-            read();
-        } else {
-            showInfo("데이터가 없음!");
+        if(requestCode == MainActivity.REQUEST_CODE_INSERT && resultCode == RESULT_OK){
+            mAdapter.swapCursor(getMemoCursor());
         }
     }
 
-    /**
-     * View 초기화
-     */
-    private void initView() {
-        editTitle = (EditText) findViewById(R.id.editTitle);
-        editContent = (EditText) findViewById(R.id.editContent);
+    private class MemoAdapter extends CursorAdapter {
 
-        btnCreate = (Button) findViewById(R.id.btnCreate);
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnUpdate = (Button) findViewById(R.id.btnUpdate);
-        btnDelete = (Button) findViewById(R.id.btnDelete);
+        public MemoAdapter(Context context, Cursor c) {
+            super(context, c, false);
+        }
 
-        textResult = (TextView) findViewById(R.id.textResult);
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+            return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView titleText = view.findViewById(android.R.id.text1);
+            titleText.setText(cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_TITLE)));
+        }
     }
-
-    /**
-     * Listener 초기화
-     */
-    private void initListener() {
-        btnCreate.setOnClickListener(this);
-        btnRead.setOnClickListener(this);
-        btnUpdate.setOnClickListener(this);
-        btnDelete.setOnClickListener(this);
-    }
-
-    /**
-     * memoDAO 초기화;
-     */
-    private void init() {
-        memoDAO = new MemoDAO(getBaseContext());
-        read();
-    }
-
 }
